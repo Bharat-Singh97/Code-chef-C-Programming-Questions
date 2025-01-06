5d4c446e535579647013e3ccabbeb3b146c981f4
Bharat-Singh97 <bs290094@gmail.com>
2025-01-06 10:43:56 +0530

    Chef has invented 1 1-minute Instant Noodles. As the name suggests, each packet takes exactly 1 1 minute to cook.
    Chef's restaurant has
    X
    X stoves and only
    1
    1 packet can be cooked in a single stove at any minute.
    
    How many customers can Chef serve in
    Y
    Y minutes if each customer orders exactly
    1
    1 packet of noodles?
    
    Input Format
    The first and only line of input contains two space-separated integers
    X
    X and
    Y
    Y — the number of stoves and the number of minutes, respectively.
    Output Format
    Print a single integer, the maximum number of customers Chef can serve in
    Y
    Y minutes
    Constraints
    1
    ≤
    X
    ,
    Y
    ≤
    1000
    1≤X,Y≤1000

---
+++ ChefandInstantNoodles.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+ int x,y , ans;
+ printf("Enter numbers");
+ scanf("%d %d", &x, &y);
+ ans = x * y;
+ 
+ printf("%d", ans);
+ return 0;
+}
+
