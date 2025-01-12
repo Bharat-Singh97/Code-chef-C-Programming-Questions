9f360fade5cfd1c7805f78cad57e7692bea53daa
Bharat-Singh97 <bs290094@gmail.com>
2025-01-12 21:15:34 +0530

    Explanation: a = 5, b = 9 and a + b = 14. Since no 'space' or next line definition has been defined in the print statement, our output becomes 5914.

---
+++ multiplechoiceQs.c
@@ -0,0 +1,10 @@
+//What will be the output of this code, if the a = 5 and b = 9 based on user input?
+
+#include <stdio.h>
+
+int main() {
+  int a, b;
+  printf("Entert number a and b :");
+  scanf("%d%d", &a, &b);
+  printf("%d%d%d", a, b, a + b); //5914
+}
